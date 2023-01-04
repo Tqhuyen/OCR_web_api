48f4006be80a26252a3c470547ff41bd312a635e
TRAN QUANG HUYEN <huyenquangtran2002@gmail.com>
2023-01-04 15:09:48 +0700

    add

--- ocr_api.py
+++ ocr_api.py
@@ -4,9 +4,16 @@ import io
 from base64 import encodebytes
 import subprocess
 from paddleocr import PaddleOCR, draw_ocr
+import calendar
+import time
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
+domain = os.environ['DOMAIN']
 def get_response_image(image_path):
     pil_img = Image.open(image_path, mode='r') # reads the PIL image
     byte_arr = io.BytesIO()
@@ -32,7 +39,7 @@ def read_text_file(file_dir: str)->list:
     result.append(int_box)
   return result
 
-def predict(img_path, use_gpu=True):
+def predict(img_path,name, use_gpu=True):
   ocr = PaddleOCR(use_gpu=use_gpu , use_angle_cls=True, lang='en') # need to run only once to download and load model into memory
   # img_path = './test_data/test_data2.png'
   result_ocr = ocr.ocr(img_path, cls=True)
@@ -48,24 +55,30 @@ def predict(img_path, use_gpu=True):
   scores = [line[1][1] for line in feature]
   im_show = draw_ocr(image, boxes, txts, scores, font_path='E:\Hekate\OCR_Project\web_api\simfang.ttf')
   im_show = Image.fromarray(im_show)
-  im_show.save('result.jpg')
+  image_result_path = f'./static/result_{name}.jpg'
+  im_show.save(image_result_path)
   return im_show, txts
 
 @app.route("/predict", methods=["POST"])
 def process_image():
+    current_GMT = time.gmtime()
+
+    time_stamp = calendar.timegm(current_GMT)
     file = request.files['image']
-    file.save('im-received.jpg')
+    img_path = f'./static/im_received_{time_stamp}.jpg'
+    file.save(img_path)
     gpu_use = request.values['gpu_use']
 
     # Read the image via file.stream
     # img = Image.open(file.stream)
-    image, message = predict(img_path='im-received.jpg', use_gpu=bool(gpu_use))
-    image_path = '.\\result.jpg' # point to your image location
-    encoded_img = get_response_image(image_path)
+    image, message = predict(img_path=img_path,name=time_stamp, use_gpu=bool(gpu_use))
+    image_result_path = f'result_{time_stamp}.jpg' # point to your image location
+    image_url = config.domain +'/static/' + image_result_path
+    encoded_img = get_response_image('./static/' + image_result_path)
     my_message = message # create your message as per your need
 
 
-    response =  { 'Status' : 'Success', 'Text': my_message , 'ImageBytes': encoded_img}
+    response =  { 'Status' : 'Success', 'Text': my_message , 'ImageURL': image_url}
     return jsonify(response) # send the result to client
 
 
